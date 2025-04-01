1aaf9b05f54415fc37ec3834a031580b54343e19
unknown <abregonair@gmail.com>
2025-03-31 18:36:08 -0700

    Installed dependecies

--- bot.py
+++ bot.py
@@ -1,1 +1,8 @@
-print("NudgeBot is alive.")
+import slack
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+client.chat_postMessage(channel='#bot', text="Nair, Please report to Discord")
