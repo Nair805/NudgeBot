67a754ba4d905e98baac6a397ebd6627ad01d33a
unknown <abregonair@gmail.com>
2025-04-05 17:44:27 -0700

    Updated Flask route to handle Slack events and detect questions

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from bot import detect_question, send_message
 
 app = Flask(__name__)
 
@@ -13,7 +14,23 @@ def slack_events():
     # Handle incoming events (basic structure)
     if data.get("type") == "event_callback":
         event = data.get("event")
+
+         # Ignore messages from bots (including this one)
+        if event.get("bot_id"):
+            return "", 200
+        
+        text = event.get("text")
         print("Received event:", event)
+        # print("This is the message:" , text)
+
+        # Detects if incoming message is a question
+        if text:
+            is_question = detect_question(text)
+            if is_question:
+                print("Alert! Message: ", text, " is a question.")
+                send_message(event.get("channel"), "Alert! Message: " +  text + " is a question.")
+            else:
+                print("Message received, but not a question:", text)
 
     return "", 200
 
